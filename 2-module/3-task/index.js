f4f568717d955c8d9194e877e5d80f363f079bdb
sveta321 <andreevasveta87@gmail.com>
2025-12-22 23:24:13 +0300

    Решены задачи модуля 2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,24 @@
 let calculator = {
-  // ваш код
+num1: 0,
+num2: 0,
+
+  read(a, b) {
+   this.num1 = a, this.num2 = b;
+  },
+
+    mul() {
+   return this.num1 * this.num2;
+  },
+
+    sum() {
+   return this.num1 + this.num2;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
+
+
+// Доп ДЗ
+// Решить через reduce
+// https://learn.javascript.ru/array-methods#reduce-reduceright
