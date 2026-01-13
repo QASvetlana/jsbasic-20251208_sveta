9330b7a706ebf4b30f4a24b33a0ffa65d19bc013
sveta321 <andreevasveta87@gmail.com>
2026-01-13 23:23:56 +0300

    реализована функция makeFriendsList(friends)

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,10 +1,10 @@
-function getMinMax(str) {
-  let arr = str.split(' ');
+function getMinMax(str) { 
+  let arr = str.split(' '); 
   let numbers = arr
     .filter(item => !isNaN(parseFloat(item)))
     .map(item => parseFloat(item)); 
   
-  let max = Math.max(...numbers);
+  let max = Math.max(...numbers); 
   let min = Math.min(...numbers);
   
   return { min: min, max: max };
--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
-function makeFriendsList(friends) {
-  // ваш код...
+function makeFriendsList(friends) { 
+  let friendsList = document.createElement('UL'); 
+  friends.forEach(function(friend) { 
+    let friendy = document.createElement('LI');
+    friendy.textContent = friend.firstName + " " + friend.lastName; 
+    friendsList.appendChild(friendy);
+  })
+return friendsList;
 }
+
+
